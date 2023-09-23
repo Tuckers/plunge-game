e1433d20708753a7c70f6c8292a80e40c3438d64
Tucker Spofford <tuckers@gmail.com>
2023-09-22 23:08:41 -0700

    adds scoring, collisions, and ship rotation

--- src/main.c
+++ src/main.c
@@ -18,6 +18,8 @@ typedef struct Gate {
     int gap;
     int gapPosition;
     int maxSpeed;
+    bool belowPlayer;
+    bool hit;
 } Gate;
 
 typedef struct Ship {
@@ -25,14 +27,38 @@ typedef struct Ship {
     Vector2 velocity;
     float rotation;
     Color color;
+    Vector2 p1;
+    Vector2 p2;
+    Vector2 p3;
 } Ship;
 
-void DrawGate(Gate gate){
-    DrawRectangle( 0, gate.position.y, gate.gapPosition, 20, gate.color );
-    DrawRectangle( gate.gapPosition + gate.gap, gate.position.y, SCREENWIDTH - (gate.gapPosition + gate.gap), 20, gate.color );
+void DrawGate(Gate *gate)
+{
+    Color color = gate->hit ? RED : gate->color;
+    DrawRectangle( 0, gate->position.y, gate->gapPosition, 20, color );
+    DrawRectangle( gate->gapPosition + gate->gap, gate->position.y, SCREENWIDTH - (gate->gapPosition + gate->gap), 20, color );
 }
 
-void UpdateShip(Ship *ship){
+void UpdateGate(Gate *gate)
+{
+    if (gate->position.y < 0)
+    {
+        gate->position.y = SCREENHEIGHT;
+        gate->belowPlayer = true;
+        gate->hit = false;
+    }
+    
+    // adjust gate velocity
+    if (gate->velocity.y > -10) gate->velocity.y -= 0.3f;
+    // update gate position
+    gate->position.y += gate->velocity.y;
+}
+
+void UpdateShip(Ship *ship)
+{
+    const float halfHeight = SHIPHEIGHT / 2;
+    const float halfWidth = SHIPWIDTH / 2;
+    const int rotationOffset = 0;
     // apply inertia
     if (ship->velocity.x > 0.1f) ship->velocity.x -= INERTIA;
     if (ship->velocity.x < -0.1f) ship->velocity.x += INERTIA;
@@ -44,18 +70,28 @@ void UpdateShip(Ship *ship){
     // check for out of bounds
     if (ship->position.x < (float)SHIPWIDTH / 2) ship->position.x = (float)SHIPWIDTH / 2;
     if (ship->position.x > (float)SCREENWIDTH - SHIPWIDTH / 2) ship->position.x = (float)SCREENWIDTH - SHIPWIDTH / 2;
+    // rotate triangle points
+    const Vector2 v1 = Vector2Rotate((Vector2){ -halfWidth, -halfHeight - rotationOffset }, DEG2RAD * ship->rotation);
+    const Vector2 v2 = Vector2Rotate((Vector2){ 0.0f, halfHeight - rotationOffset}, DEG2RAD * ship->rotation);
+    const Vector2 v3 = Vector2Rotate((Vector2){ halfWidth, -halfHeight - rotationOffset}, DEG2RAD * ship->rotation);
+    // add position and update ship values
+    ship->p1 = Vector2Add(v1, ship->position);
+    ship->p2 = Vector2Add(v2, ship->position);
+    ship->p3 = Vector2Add(v3, ship->position);
+}
+
+bool checkCollision(Gate *gate, Ship *ship){
+        Vector2 collisionPoint;
+        // check to see if left or right side of triangle contact gate
+        const bool leftCheck = CheckCollisionLines(ship->p1, ship->p2, gate->position, (Vector2){gate->gapPosition, gate->position.y}, &collisionPoint);
+        const bool rightCheck = CheckCollisionLines(ship->p3, ship->p2, (Vector2){gate->gapPosition + gate->gap, gate->position.y}, (Vector2){SCREENWIDTH, gate->position.y}, &collisionPoint);
+        return leftCheck || rightCheck;
 
 }
 
 void DrawShip(Ship *ship){
-    const float halfHeight = SHIPHEIGHT / 2;
-    const float halfWidth = SHIPWIDTH / 2;
-    // Rotate triangle points
-    const Vector2 v1 = Vector2Rotate((Vector2){ -halfWidth, -halfHeight }, DEG2RAD * ship->rotation);
-    const Vector2 v2 = Vector2Rotate((Vector2){ 0.0f, halfHeight }, DEG2RAD * ship->rotation);
-    const Vector2 v3 = Vector2Rotate((Vector2){ halfWidth, -halfHeight }, DEG2RAD * ship->rotation);
-    // Add position and draw triangle
-    DrawTriangle( Vector2Add(v1, ship->position), Vector2Add(v2, ship->position), Vector2Add(v3, ship->position), ship->color);
+    // Draw triangle
+    DrawTriangle( ship->p1, ship->p2, ship->p3, ship->color);
 }
 
 
@@ -67,12 +103,13 @@ int main(void)
     const int screenHeight = SCREENHEIGHT;
     const int screenWidth = SCREENWIDTH;
     float distance = 0.0f;
+    int score = 0;
 
     Ship playerShip = { 
         {(float)screenWidth / 2 , SHIPVERTICALPOSITION},
         {0.0f , 0.0f},
         0.0f,
-        RED,
+        RED
      };
 
     Gate gates[MAX_GATES] = { 0 };
@@ -86,6 +123,8 @@ int main(void)
         gates[i].position.y = (float)screenHeight;
         gates[i].velocity.x = 0.0f;
         gates[i].velocity.y = 0.0f;
+        gates[i].belowPlayer = true;
+        gates[i].hit = false;
     }
 
     InitWindow(screenWidth, screenHeight, "plunge – game");
@@ -99,10 +138,6 @@ int main(void)
     // MAIN LOOP ----------------------------------------------------------------------------------------//
     while (!WindowShouldClose())        // Detect window close button or ESC key
     {
-        // UPDATE PLAYER MOVEMENT
-
-        if (gates[0].position.y < 0) gates[0].position.y = screenHeight;
-        
         distance += 5;
 
         // add input to velocity
@@ -115,11 +150,25 @@ int main(void)
             if (playerShip.rotation < 60) playerShip.rotation += 1.0f;
         }
 
+        // update player and gates
         UpdateShip( &playerShip );
+        UpdateGate( &gates[0] );
+        
+        // check current gate for collisions
+        bool collision = checkCollision(&gates[0], &playerShip);
 
-        // gate velocity
-        if (gates[0].velocity.y > -10) gates[0].velocity.y -= 0.3f;
-        gates[0].position.y += gates[0].velocity.y;
+        // update score
+        if (collision && gates[0].belowPlayer) 
+        {
+            gates[0].hit = true;
+            gates[0].belowPlayer = false;
+            score--;
+        }
+        if (gates[0].position.y < playerShip.position.y && gates[0].belowPlayer)
+        {
+            gates[0].belowPlayer = false;
+            score++;
+        }
 
         // DRAW FRAME
         BeginDrawing();
@@ -129,10 +178,12 @@ int main(void)
             BeginMode2D(camera);
 
                 DrawShip( &playerShip );
-                DrawGate( gates[0] ); 
+                DrawGate( &gates[0] ); 
 
             EndMode2D();  
             DrawText(TextFormat("%000000009.0f M", distance), 450, 96, 36, WHITE);
+            DrawText(TextFormat("SCORE: %d00", score), 450, 150, 36, WHITE);
+            DrawText(TextFormat("Collision: %s", collision ? "true" : "false"), 10, 10, 24, collision ? RED : WHITE);
 
         EndDrawing();
     }
